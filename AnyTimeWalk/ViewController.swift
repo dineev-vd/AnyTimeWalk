f48993cafec2ffdc208b360622277980a233a587
Vladislav Dineev <vddineev@edu.hse.ru>
2021-03-23 10:41:42 +0300

    Added nav controller

---
+++ AnyTimeWalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnyTimeWalk
+//
+//  Created by Влад Динеев on 24.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
