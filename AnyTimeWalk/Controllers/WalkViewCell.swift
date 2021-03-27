54de1b00f2e28ff10621c29ed5b78dd3e492185e
Egor Dadugin <mkrs95@yandex.ru>
2021-03-28 02:54:01 +0300

    Favorites view collection added

--- AnyTimeWalk/Controllers/WalkViewCell.swift
+++ AnyTimeWalk/Controllers/WalkViewCell.swift
@@ -16,11 +16,19 @@ class WalkViewCell: UICollectionViewCell {
     var delegate:WalkCollectionCellDelegate?
     
     @IBOutlet weak var walkImage: UIImageView!
+    @IBOutlet weak var favsImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var favsName: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var favsDescription: UILabel!
     @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var favsLike: UIButton!
     @IBOutlet weak var likeButtonTapped: UIButton!
+    @IBOutlet weak var favsButtonTapped: UIButton!
     @IBAction func likeButtonPressed(_ sender: Any) {
+        delegate?.didLikeButtonPressed(cell: self)
+    }
+    @IBAction func favsButtonPressed(_ sender: Any) {
         delegate?.didLikeButtonPressed(cell: self)
     }
     
@@ -31,6 +39,17 @@ class WalkViewCell: UICollectionViewCell {
             }
             else{
                 likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+            }
+        }
+    }
+    
+    var favsIsLiked = false{
+        didSet{
+            if favsIsLiked{
+                favsLike.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            }
+            else{
+                favsLike.setImage(UIImage(systemName: "heart"), for: .normal)
             }
         }
     }
--- AnyTimeWalk/FavouritesViewController.swift
+++ AnyTimeWalk/FavouritesViewController.swift
@@ -6,16 +6,71 @@
 //
 
 import UIKit
+import CoreData
 
-class FavouritesViewController: UIViewController {
-
-    @IBAction func printFavs(_ sender: Any) {
-        print(favs)
+class FavouritesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, WalkCollectionCellDelegate {
+    var container:NSPersistentContainer!
+    var tours: [Tour] = []
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func didLikeButtonPressed(cell: WalkViewCell) {
+        if let indexPath = collectionView.indexPath(for: cell) {
+            favs[indexPath.row].isLiked = favs[indexPath.row].isLiked ? false : true
+            cell.isLiked = favs[indexPath.row].isLiked
+            favs.remove(at: indexPath.row)
+            //Остается лайк в walks
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! WalkViewCell
+        cell.favsName.text = favs[indexPath.row].name
+        cell.favsImage.image = favs[indexPath.row].featuredImage
+        cell.favsDescription.text = favs[indexPath.row].description
+        cell.favsIsLiked = favs[indexPath.row].isLiked
+        cell.delegate = self
+        cell.layer.cornerRadius = 4.0
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let controller = tabBarController?.storyboard?.instantiateViewController(identifier: "InfoViewController") as? InfoViewController{
+            controller.currentWalk = favs[indexPath.row]
+            tabBarController?.present(controller, animated: true)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(favs)
+        container = NSPersistentContainer(name: "AnyTimeWalk")
+        super.viewDidLoad()
+        collectionView.backgroundColor = .clear
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        collectionView.reloadData()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return favs.count
+    }
+    
+    func loadSavedData() {
+        let request = Tour.createFetchRequest()
+        let sort = NSSortDescriptor(key: "name", ascending: false)
+        request.sortDescriptors = [sort]
+        do {
+            tours = try container.viewContext.fetch(request)
+            print("Got \(tours.count) commits")
+            collectionView.reloadData()
+        } catch {
+            print("Fetch failed")
+        }
+    }
 }
